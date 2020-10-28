f60649247d75234ea25ef8f33ea00a85b6419a1b
Wendy Boeker <wendyboeker@khanacademy.org>
2020-10-28 12:14:33 -0700

    adding slackbot boilerplate

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# Marley
+# Build a Slack Bot with Node.js on Cloud Run
+
+This repository contains the source code for the [Build a Slack Bot with Node.js on Cloud Run
+Google codelab][cloud-slack-bot-codelab].
+
+[![Build Status][travis-status-image]][travis-status]
+
+[cloud-slack-bot-codelab]: https://codelabs.developers.google.com/codelabs/cloud-slack-bot/
+[travis-status]: https://travis-ci.org/googlecodelabs/cloud-slack-bot
+[travis-status-image]: https://travis-ci.org/googlecodelabs/cloud-slack-bot.svg?branch=master
+
+## Resources
+
+### Codelabs and Samples
+
+- [Google codelabs][codelabs]
+- [Slack integrations on Google Cloud Platform samples][slack-samples]
+- [Node.js on Google Cloud Platform samples][nodejs-samples]
+
+[codelabs]: https://g.co/codelabs
+[nodejs-samples]: https://github.com/GoogleCloudPlatform/nodejs-docs-samples
+[slack-samples]: https://github.com/GoogleCloudPlatform/slack-samples
+
+### Documentation
+
+- [Google Cloud Platform][cloud]
+- [Google Cloud Run][run]
+- [Google Container Engine][container-engine]
+- [Google Container Registry][container-registry]
+- [Google Secret Manager][secret-manager]
+- [Slack API][slack-api]
+
+[cloud]: https://cloud.google.com/
+[run]: https://cloud.google.com/run
+[container-engine]: https://cloud.google.com/container-engine/
+[container-registry]: https://cloud.google.com/container-registry/
+[secret-manager]: https://cloud.google.com/secret-manager
+[slack-api]: https://api.slack.com/
+
+## License
+
+Apache Version 2.0
+
+See [LICENSE](LICENSE).
---
+++ test/marleybot_test.js
@@ -0,0 +1,66 @@
+/* *****************************************************************************
+Copyright 2016 Google LLC
+
+Licensed under the Apache License, Version 2.0 (the "License")
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+********************************************************************************
+
+This is a sample Slack bot built with Botkit.
+*/
+const assert = require('assert')
+
+/**
+ * Function to test Botkit Controller Configurations.
+ */
+function testBotkitController () {
+  const { Botkit, BotkitConversation } = require('botkit')
+  const { SlackAdapter, SlackEventMiddleware } = require(
+    'botbuilder-adapter-slack')
+
+  const adapter = new SlackAdapter({
+    enable_incomplete: true,
+    debug: true
+  })
+
+  adapter.use(new SlackEventMiddleware())
+
+  const controller = new Botkit({
+    webhook_uri: '/api/messages',
+    adapter: adapter
+  })
+
+  // test controller version
+  assert(parseInt(controller.version) >= 4)
+
+  // test webhook endpoint
+  const config = controller.getConfig()
+  assert(config.webhook_uri === '/api/messages')
+
+  // testBotkitConversation
+  const convo = new BotkitConversation('test-dialog', controller)
+  controller.addDialog(convo)
+  assert('test-dialog' in controller.dialogSet.dialogs)
+
+  controller.shutdown()
+}
+
+/**
+ * Function to test Secret Manager Client.
+ */
+function testSecretManagerClient () {
+  const { SecretManagerServiceClient } = require('@google-cloud/secret-manager')
+  const client = new SecretManagerServiceClient()
+  assert(client)
+};
+
+testBotkitController()
+testSecretManagerClient()
