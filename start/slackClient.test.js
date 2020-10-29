bf290fc7cde553a96ab5c8e768ebad94830238ae
Wendy Boeker <wendyboeker@khanacademy.org>
2020-10-29 14:48:13 -0700

    using env stored tokens

--- start/slackClient.test.js
+++ start/slackClient.test.js
@@ -1,12 +1,15 @@
+const dotenv = require("dotenv");
+
 const { SlackClient } = require("./slackClient.js");
 
+dotenv.config();
 const BASE_SLACK_URL = "https://slack.com";
-const API_TOKEN = "your-token-here";
+const BOT_TOKEN = process.env.BOT_TOKEN;
 
 it("SlackClient has a fetchUser method", async () => {
-  const client = new SlackClient(API_TOKEN, BASE_SLACK_URL);
+  const client = new SlackClient(BOT_TOKEN, BASE_SLACK_URL);
   const userData = await client.fetchUser("U0191680A49").catch((error) => {
     console.log(error);
   });
-  console.log(userData);
+  expect(userData.profile.email).toEqual("wendyboeker@khanacademy.org");
 });
