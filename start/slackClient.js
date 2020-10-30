8d392ce8da82c43a50300f860e89117af71b081f
Wendy Boeker <wendyboeker@khanacademy.org>
2020-10-29 18:18:37 -0700

    commented out token

--- start/slackClient.js
+++ start/slackClient.js
@@ -9,7 +9,7 @@ class SlackClient {
 
   async fetchUser(userID) {
     console.log("USER ID IN FETCH USER", userID);
-    console.log("TOKEN", this.apiToken.splice(-1));
+    // console.log("TOKEN", this.apiToken[20]);
     const get = bent(this.baseSlackUrl, "GET", 200);
     const response = await get("/api/users.profile.get?token=" + this.apiToken + "&user=" + userID);
     const jsonResponse = await response.json();
